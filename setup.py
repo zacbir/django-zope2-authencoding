8c86e867fb958054b779341d10089405d33e10cf
Zachery Bir <zbir@urbanape.com>
2013-06-24 14:54:29 -0400

    Make it non-zip-safe (for easier verification), and include the configure.zcml as a data file.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version="0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    data_files=[('djangoauthencoding/main', ['src/djangoauthencoding/main/configure.zcml'])],
     namespace_packages=["djangoauthencoding"],
     install_requires=["setuptools",
                       "Zope2"],
+    zip_safe=False,
 )
